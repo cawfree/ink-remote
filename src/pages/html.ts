3ce8df3908e47dc89fc58ebbbb6c845bbbfa10fe
cawfree <hello@cawfree.com>
2022-08-16 22:48:33 +0100

    chore: html

--- src/pages/html.ts
+++ src/pages/html.ts
@@ -6,28 +6,18 @@ export const html = ({address, port}: {
 <html>
  <head>
   <script type = "text/javascript">
-   function WebSocketTest() {
-    if ("WebSocket" in window) {
-     // Let us open a web socket
-     var ws = new WebSocket("ws://${address}:${String(port)}");
-     
-     ws.onopen = function () {
-       console.log("Sent.")
-       ws.send("Hello, world.");
-     };
-     
-     ws.onmessage = console.log;
-     ws.onclose = console.log;
-    } else {
-      console.error("WebSockets are not supported by your browser.");
-    }
-   }
+    var ws = new WebSocket("ws://${address}:${String(port)}");
+    
+    ws.onopen = function () {
+      ws.send("Hello, world.");
+    };
+    
+    ws.onmessage = console.log;
+    ws.onclose = console.log;
+    
   </script>
  </head>
  <body>
-  <div id = "sse">
-     <a href = "javascript:WebSocketTest()">Run WebSocket</a>
-  </div>
  </body>
 </html>
 `.trim();
