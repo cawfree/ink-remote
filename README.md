35afb0fa9ad978b2e19a6397da0ccc3ee9a42988
cawfree <hello@cawfree.com>
2022-08-17 00:40:14 +0100

    chore: node_modules

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Serve an [__Ink__](https://github.com/vadimdemedes/ink) application via [__Expre
 Install using [__Yarn__](https://yarnpkg.com/):
 
 ```shell
-yarn add ink-remote
+yarn add ink-remote xterm
 ```
 
 Then just swap out `ink`'s included `render` function for the alternative `render` method exported by `ink-remote`:
@@ -24,7 +24,7 @@ import * as React from "react";
 + render(<Text children="Hello, world!" />, {port: 3000});
 ```
 
-In this instance, not only will your application will be rendered in the CLI like usual, but it will also be accessible via [`http://localhost:3000`](http://localhost:3000`).
+In this instance, not only will your application will be rendered in the CLI like usual, but it will also be accessible via [`http://localhost:3000`](http://localhost:3000`). The content is rendered using [__XTerm__](https://github.com/xtermjs/xterm.js/), whose fully-featured renderer is capable of rendering your application faithfully.
 
 ### ✌️ license
 [__MIT__](./LICENSE)
--- src/utils/serve.ts
+++ src/utils/serve.ts
@@ -3,29 +3,35 @@ import expressWs from "express-ws";
 import * as handlebars from "handlebars";
 import fs from "fs";
 import ip from "ip";
-import path from "path";
+import appRootPath from "app-root-path";
 
 export const serve = ({port}: {
   readonly port: number;
 }) => {
   const {app, getWss} = expressWs(
     express()
-      .use(express.static(__dirname + '/public'))
+      .use(express.static(appRootPath.resolve('public')))
       .get('/index.css', (req, res) =>
-        res.sendFile(path.resolve('public/index.css'))
+        res.sendFile(appRootPath.resolve('node_modules/ink-remote/public/index.css'))
       )
       .get('/node_modules/xterm/lib/xterm.js', (req, res) =>
-        res.sendFile(path.resolve('node_modules/xterm/lib/xterm.js'))
+        res.sendFile(appRootPath.resolve('node_modules/xterm/lib/xterm.js'))
       )
       .get( '/', (e, res) => res
         .status(200)
         .send(
-          handlebars.compile(fs.readFileSync('public/index.html', 'utf-8'))({
-            address: ip.address(),
-            port,
-          }),
-        ),
-      )
+          handlebars.compile(
+            fs.readFileSync(
+              appRootPath.resolve(
+                'node_modules/ink-remote/public/index.html'),
+                'utf-8',
+            )
+            )({
+                address: ip.address(),
+                port,
+              }),
+         ),
+      ),
   );
 
   // Receive messages from the client.
